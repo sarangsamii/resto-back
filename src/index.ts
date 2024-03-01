2d36c7a3b2333be81af56829b0a2f57a50011211
Sarang <sarangsami75@gmail.com>
2024-03-01 19:31:32 +0400

    feat: :tada: Initial Project with Express.js
    Using Typescript

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response, Application } from "express";
+var createError = require('http-errors');
+import dotenv from "dotenv";
+
+//For env File
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({name:"Bahare"});
+});
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
